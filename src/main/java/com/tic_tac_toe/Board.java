a697eab126ae3c49cf06a2b37c840a9507515c90
annahico <annahico@gmail.com>
2026-01-27 15:07:25 +0100

    merged pull

--- src/main/java/com/tic_tac_toe/Board.java
+++ src/main/java/com/tic_tac_toe/Board.java
@@ -2,19 +2,17 @@ package com.tic_tac_toe;
 
 public class Board {
 
-  int rows;
-  int columns;
-  String [][] board; 
+    int rows;
+    int columns;
+    String[][] board;
 
+    public Board(int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        this.board = new String[rows][columns];
 
-  public Board (int rows, int columns){
-    this.rows = rows;
-    this.columns = columns;
-    this.board = new String [rows][columns]; 
-
-    // for (int rows = 0 ; rows < board.length ; linesNumber++) {
-            //for ( int columnsNumber = 0 ; columnsNumber < integers[linesNumber].length; columnsNumber++)*/
-
-  }
+        // for (int rows = 0 ; rows < board.length ; linesNumber++) {
+        //for ( int columnsNumber = 0 ; columnsNumber < integers[linesNumber].length; columnsNumber++)*/
+    }
 
 }
